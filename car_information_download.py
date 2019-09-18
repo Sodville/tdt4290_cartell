47e33bf5ad5b90e45eff2c79e5721f10b22ea471
uyth <uyth95@gmail.com>
2019-09-18 14:46:22 +0200

    Add script that downloads car information from license number

---
+++ car_information_download.py
@@ -0,0 +1,35 @@
+from os import listdir
+from os.path import isfile, join
+import time
+import car_information_service
+import json
+
+def get_filenames_in_directory(directory):
+    return [f.split(".")[0] for f in listdir(directory) if isfile(join(directory, f))]
+
+
+def dump_json_to_file(directory, filepath, data):
+    with open(join(directory, filepath + ".json"), "w") as outfile:
+        outfile.write(json.dumps(data))
+
+output_directory = "datasets/car_data"
+img_directory = "datasets/car_images"
+license_numbers = get_filenames_in_directory(img_directory)
+for license_number in license_numbers:
+    if not isfile(join(output_directory, license_number + ".json")):
+        print(license_number)
+        hasFetched = False
+        try:
+            car = car_information_service.get_car_information_from_api(license_number)
+            hasFetched = True
+        except:
+            try:
+                car = car_information_service.scrape_from_regnr(license_number)
+                hasFetched = True
+            except:
+                print("Could not fetch information about :", license_number)
+        finally:
+            if hasFetched:
+                dump_json_to_file(output_directory, license_number, car)
+                print(json.dumps(car))
+            time.sleep(1)
