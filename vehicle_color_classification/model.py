e5507360fdbcf65da55252b0eb266cfe47c0bfcc
Marvin Reza <marvinreza@outlook.com>
2019-09-18 14:46:44 +0200

    fix model w.r.t paper

--- vehicle_color_classification/model.py
+++ vehicle_color_classification/model.py
@@ -14,35 +14,29 @@ def make_model(input_shape, num_classes):
     top_conv1 = MaxPooling2D(pool_size=(3,3), strides=(2,2))(top_conv1)
 
     # second top convolution layer
-    # split feature map by half
-    top_top_conv2 = Lambda(lambda x : x[:,:,:,:24])(top_conv1)
-    top_bot_conv2 = Lambda(lambda x : x[:,:,:,24:])(top_conv1)
-
-    top_top_conv2 = Conv2D(filters=64, kernel_size=(3,3),activation='relu',padding='same')(top_top_conv2)
+    top_top_conv2 = Conv2D(filters=64, kernel_size=(3,3),activation='relu',padding='same')(top_conv1)
     top_top_conv2 = BatchNormalization()(top_top_conv2)
     top_top_conv2 = MaxPooling2D(pool_size=(3,3),strides=(2,2))(top_top_conv2)
 
-    top_bot_conv2 = Conv2D(filters=64, kernel_size=(3,3), activation='relu',padding='same')(top_bot_conv2)
+    # second bot convolution layer
+    top_bot_conv2 = Conv2D(filters=64, kernel_size=(3,3), activation='relu',padding='same')(top_conv1)
     top_bot_conv2 = BatchNormalization()(top_bot_conv2)
     top_bot_conv2 = MaxPooling2D(pool_size=(3,3), strides=(2,2))(top_bot_conv2)
 
     # third top convolution layer
-    # concat 2 feature map
-    top_conv3 = Concatenate()([top_top_conv2,top_bot_conv2])
+    top_conv3 = Concatenate()([top_top_conv2, top_bot_conv2])
     top_conv3 = Conv2D(filters=192,kernel_size=(3,3),activation='relu',padding='same')(top_conv3)
 
     # fourth top convolution layer
-    # split feature map by half
-    top_top_conv4 = Lambda(lambda x : x[:,:,:,:96])(top_conv3)
-    top_bot_conv4 = Lambda(lambda x : x[:,:,:,96:])(top_conv3)
-
-    top_top_conv4 = Conv2D(filters=96, kernel_size=(3,3), activation='relu', padding='same')(top_top_conv4)
-    top_bot_conv4 = Conv2D(filters=96, kernel_size=(3,3), activation='relu', padding='same')(top_bot_conv4)
+    top_top_conv4 = Conv2D(filters=96, kernel_size=(3,3), activation='relu', padding='same')(top_conv3)
+    # fourth bot convolution layer
+    top_bot_conv4 = Conv2D(filters=96, kernel_size=(3,3), activation='relu', padding='same')(top_conv3)
 
     # fifth top convolution layer
     top_top_conv5 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(top_top_conv4)
     top_top_conv5 = MaxPooling2D(pool_size=(3,3),strides=(2,2))(top_top_conv5) 
 
+    # fifth bot convolution layer
     top_bot_conv5 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(top_bot_conv4)
     top_bot_conv5 = MaxPooling2D(pool_size=(3,3), strides=(2,2))(top_bot_conv5)
 
@@ -53,31 +47,21 @@ def make_model(input_shape, num_classes):
     bottom_conv1 = MaxPooling2D(pool_size=(3,3), strides=(2,2))(bottom_conv1)
 
     # second bottom convolution layer
-
-    # split feature map by half
-    bottom_top_conv2 = Lambda(lambda x : x[:,:,:,:24])(bottom_conv1)
-    bottom_bot_conv2 = Lambda(lambda x : x[:,:,:,24:])(bottom_conv1)
-
-    bottom_top_conv2 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(bottom_top_conv2)
+    bottom_top_conv2 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(bottom_conv1)
     bottom_top_conv2 = BatchNormalization()(bottom_top_conv2)
     bottom_top_conv2 = MaxPooling2D(pool_size=(3,3), strides=(2,2))(bottom_top_conv2)
 
-    bottom_bot_conv2 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(bottom_bot_conv2)
+    bottom_bot_conv2 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(bottom_conv1)
     bottom_bot_conv2 = BatchNormalization()(bottom_bot_conv2)
     bottom_bot_conv2 = MaxPooling2D(pool_size=(3,3), strides=(2,2))(bottom_bot_conv2)
 
     # third bottom convolution layer
-    # concat 2 feature map
     bottom_conv3 = Concatenate()([bottom_top_conv2, bottom_bot_conv2])
     bottom_conv3 = Conv2D(filters=192, kernel_size=(3,3), activation='relu', padding='same')(bottom_conv3)
 
     # fourth bottom convolution layer
-    # split feature map by half
-    bottom_top_conv4 = Lambda(lambda x : x[:,:,:,:96])(bottom_conv3)
-    bottom_bot_conv4 = Lambda(lambda x : x[:,:,:,96:])(bottom_conv3)
-
-    bottom_top_conv4 = Conv2D(filters=96, kernel_size=(3,3), activation='relu', padding='same')(bottom_top_conv4)
-    bottom_bot_conv4 = Conv2D(filters=96, kernel_size=(3,3), activation='relu', padding='same')(bottom_bot_conv4)
+    bottom_top_conv4 = Conv2D(filters=96, kernel_size=(3,3), activation='relu', padding='same')(bottom_conv3)
+    bottom_bot_conv4 = Conv2D(filters=96, kernel_size=(3,3), activation='relu', padding='same')(bottom_conv3)
 
     # fifth bottom convolution layer
     bottom_top_conv5 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(bottom_top_conv4)
@@ -100,3 +84,8 @@ def make_model(input_shape, num_classes):
     model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
     
     return model
+
+if __name__ == "__main__":
+    from keras.utils import plot_model
+    model = make_model((227,227,3), 8)
+    plot_model(model, to_file="model_graph.png")
