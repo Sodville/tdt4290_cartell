03cb2c1064b49c8bc11713245648565dbbf37b1d
uyth <uyth95@gmail.com>
2019-09-18 16:55:27 +0200

    Add method that loads car data from json

--- Car.py
+++ Car.py
@@ -11,3 +11,10 @@ class Car:
 
     def __repr__(self):
         return f'Car({self.registreringsnummer!r}, {self.merke!r}, {self.modell!r}, {self.farge!r})'
+
+    def load_data_from_json(self, data):
+        car_data = json.loads(data)
+        self.registreringsnummer = car_data["registreringsnummer"]
+        self.merke = car_data["merke"]
+        self.modell = car_data["modell"]
+        self.farge = car_data["farge"]
