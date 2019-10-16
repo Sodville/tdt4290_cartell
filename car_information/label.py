1d9f2d1fcb62311a29941fe6454a36cde8dc3e63
uyth <uyth95@gmail.com>
2019-10-16 18:54:42 +0200

    Refactor names

--- car_information/label.py
+++ car_information/label.py
@@ -26,52 +26,52 @@ def init_output_directory(directory):
     safe_directory(directory + "/unlabelled")
 
 
-def label_image(car, image, input_directory, output_directory):
+def label_image(car, image, source_directory, destination_directory):
     print("Label brand")
-    label_brand(car, image, input_directory, output_directory)
+    label_brand(car, image, source_directory, destination_directory)
     print("Label color")
-    label_color(car, image, input_directory, output_directory)
+    label_color(car, image, source_directory, destination_directory)
     print("Label model")
-    label_model(car, image, input_directory, output_directory)
+    label_model(car, image, source_directory, destination_directory)
 
 
-def label_brand(car, image, input_directory, output_directory):
+def label_brand(car, image, source_directory, destination_directory):
     assert car["merke"]
-    destination_path = output_directory + "/brand/" + car["merke"] + "/"
-    copy_image(image, input_directory, destination_path)
+    destination_directory += "/brand/" + car["merke"] + "/"
+    copy_image(image, source_directory, destination_directory)
 
 
-def label_color(car, image, input_directory, output_directory):
+def label_color(car, image, source_directory, destination_directory):
     assert car["farge"]
-    destination_path = output_directory + "/color/" + car["farge"] + "/"
-    copy_image(image, input_directory, destination_path)
+    destination_directory += "/color/" + car["farge"] + "/"
+    copy_image(image, source_directory, destination_directory)
 
 
-def label_model(car, image, input_directory, output_directory):
+def label_model(car, image, source_directory, destination_directory):
     assert car["modell"]
-    destination_path = output_directory + "/model/" + car["modell"] + "/"
-    copy_image(image, input_directory, destination_path)
+    destination_directory += "/model/" + car["modell"] + "/"
+    copy_image(image, source_directory, destination_directory)
 
 
 if __name__ == "__main__":
-    input_directory = sys.argv[1]
-    output_directory = sys.argv[2]
+    image_directory = sys.argv[1]
+    dataset_directory = sys.argv[2]
 
-    images = os.listdir(input_directory)
-    init_output_directory(output_directory)
+    images = os.listdir(image_directory)
+    init_output_directory(dataset_directory)
 
     for image in images:
         print("Identifying license plate for image " + image)
-        license_plate = identify_license_plate(input_directory, image)
+        license_plate = identify_license_plate(image_directory, image)
         if license_plate:
             print("Identified license plate: " + license_plate)
             print("Fetching data for license plate " + license_plate)
             car = get_data_from_vegvesenet(license_plate)
             print(car)
             print("Labels image into data sets")
-            label_image(car, image, input_directory, output_directory)
+            label_image(car, image, image_directory, dataset_directory)
         else:
             print("Could not identify license plate of image " + image)
-            destination = output_directory + "/unlabelled/"
-            copy_image(image, input_directory, destination)
+            destination = dataset_directory + "/unlabelled/"
+            copy_image(image, image_directory, destination)
         # delete_file(input_directory + image)
