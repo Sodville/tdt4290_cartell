04cf4d6fa03d7c5a8ac117d2966257bb04fc192c
Marvin Reza <marvinreza@outlook.com>
2019-11-20 14:49:13 +0100

    refactor api to also work with any models and labels

--- api/api_upload.py
+++ api/api_upload.py
@@ -4,9 +4,9 @@ import fleep
 
 from api_setup import api_setup
 from flask import request, jsonify
-from model_utils import predict, load_model, load_image, get_brands
+from model_utils import predict, load_model, load_image, get_labels
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"])
 
 
 def allowed_file(file):
@@ -18,48 +18,48 @@ def allowed_file(file):
         return ext and ext[1:] in ALLOWED_EXTENSIONS
 
 
-@api_setup.route('/api', methods=['POST'])
+@api_setup.route("/api", methods=["POST"])
 def upload_file():
     # check if the post request has the file part
-    if 'file' not in request.files:
-        resp = jsonify({'message': 'No file part in the request'})
+    if "file" not in request.files:
+        resp = jsonify({"message": "No file part in the request"})
         resp.status_code = 400
         return resp
-    file = request.files['file']
-    if file.filename == '':
-        resp = jsonify({'message': 'No file selected for uploading'})
+    file = request.files["file"]
+    if file.filename == "":
+        resp = jsonify({"message": "No file selected for uploading"})
         resp.status_code = 400
         return resp
     if file and allowed_file(file):
         file.seek(0) # in case file was read through earlier
         encoded_image = file.read()
-        image = load_image(encoded_image)
+        image = load_image(encoded_image, target_shape)
 
         # Here the image is fetched from the api, and correctly encoded for the model
-        pred = predict(model, image, brands)  # Predict on the model
+        pred = predict(model, image, labels)  # Predict on the model
         pred = {x: float(pred[x]) for x in pred}  # Translate np float -> python float to allow it to be made to json
         resp = jsonify(pred)  # Build response as json
         resp.status_code = 201
         return resp
     else:
         resp = jsonify(
-            {'message': 'Allowed file types are png, jpg, jpeg'})
+            {"message": "Allowed file types are png, jpg, jpeg"})
         resp.status_code = 400
         return resp
 
 
 # A liveness test to check the status of the container
-@api_setup.route("/liveness", methods=['GET'])
+@api_setup.route("/liveness", methods=["GET"])
 def liveness():
     return "OK"
 
 
 def make_argparser():
-    parser = argparse.ArgumentParser(description='Predict car brands')
-    parser.add_argument('-w', '--weights_path', default='./efficientnetb0_512.hdf5',
+    parser = argparse.ArgumentParser(description="Predict a desired car feature")
+    parser.add_argument("-w", "--weights_path", default="./efficientnetb0_512.hdf5",
             type=str, help="Path to weights for model")
-    parser.add_argument('-b', '--brands_path', default='./brands.txt', 
-            type=str, help="Path to brand labels for model")
+    parser.add_argument("-l", "--label_path", default="./brands.txt",
+            type=str, help="Path to labels for model")
     return parser
 
 
@@ -67,9 +67,10 @@ if __name__ == "__main__":
     args = make_argparser().parse_args()
 
     # Initialize the model
-    print("Loading brands and model")
-    brands = get_brands(args.brands_path)
+    print("Loading labels and model")
+    labels = get_labels(args.label_path)
     model = load_model(args.weights_path)
+    target_shape = model.layers[0].input_shape[1:-1]
     print("Model was loaded succesfully.")
 
-    api_setup.run(host="0.0.0.0")  # ssl_context='adhoc' for unsigned ssl
+    api_setup.run(host="0.0.0.0")  # ssl_context="adhoc" for unsigned ssl
--- api/model_utils.py
+++ api/model_utils.py
@@ -3,16 +3,16 @@ import efficientnet.keras as efn
 from PIL import Image
 import io
 
-def get_brands(brands_path):
-    brands = None
-    with open(brands_path, "r") as f:
-        brands = [line.strip() for line in f.readlines()]
-        brands = sorted(brands)
-    return brands
+def get_labels(labels_path):
+    labels = None
+    with open(labels_path, "r") as f:
+        labels = [line.strip() for line in f.readlines()]
+        labels = sorted(labels)
+    return labels
 
 def load_image(encoded_img, target_size=(512, 512)):
     img = Image.open(io.BytesIO(encoded_img))
-    img = img.convert('RGB')
+    img = img.convert("RGB")
     img = img.resize(target_size, Image.NEAREST)
     img_array = keras.preprocessing.image.img_to_array(img)
     return img_array
@@ -22,15 +22,15 @@ def load_model(weights_path):
     model._make_predict_function() # to fix weird Keras error with Flask about missing Tensor
     return model
 
-def decode_prediction(brands, pred):
+def decode_prediction(labels, pred):
     decoded_pred = dict()
     for i, prob in enumerate(pred):
-        decoded_pred[brands[i]] = prob
+        decoded_pred[labels[i]] = prob
     return decoded_pred
 
-def predict(model, image, brands):
+def predict(model, image, labels):
     image = image / 255.
     pred = model.predict(image[None, :, :, :])
-    decoded_pred = decode_prediction(brands, pred[0])
+    decoded_pred = decode_prediction(labels, pred[0])
     print(decoded_pred)
     return decoded_pred
